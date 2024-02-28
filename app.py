96004e70bf7ceced8d5f8a3e56d63f504c5fdc0f
Ayush Bhardwaj <100165141+Ayush-corp@users.noreply.github.com>
2024-02-27 20:26:44 -0800

    Eliminated the unnecessary files

--- app.py
+++ app.py
@@ -66,4 +66,5 @@ def index():
             return render_template('index.html', error=error_message)
     return render_template('index.html')
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
